b09ad716c2b942ea5c75839320c94e51a22caaff
anikchowdhury440 <anikchowdhury440@gmail.com>
2020-09-03 20:26:08 +0530

    Compute arithmetic operation c+a/b

--- sortingArithmeticComputation.sh
+++ sortingArithmeticComputation.sh
@@ -6,6 +6,8 @@ read -p "Enter Third Number: " c
 
 arith_computation1=$(( $a+$b*$c ))
 arith_computation2=$(( $a*$b+$c ))
+arith_computation3=$(( $c+$a/$b ))
 
 echo $arith_computation1
 echo $arith_computation2
+echo $arith_computation3
